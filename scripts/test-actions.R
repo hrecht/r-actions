7a64022c4675641c7b4ea7e85922159b3a8cc77f
Hannah Recht <hrecht@users.noreply.github.com>
2025-08-12 17:10:06 -0400

    init

---
+++ scripts/test-actions.R
@@ -0,0 +1,13 @@
+# Test out passing data from R to Github Env
+
+# Final result should look like:
+# 'echo "POST_BSKY=false" >> "$GITHUB_ENV"'
+send_env <- function(var_name, var_value) {
+	post_part <- paste0(var_name, "='", var_value, "'", ' >> "$GITHUB_ENV"')
+	print(post_part)
+	system(paste('echo ', post_part))
+}
+
+send_env("TEXT_TEST", "Hello world")
+send_env("BOOLEAN_TEST", TRUE)
+send_env("NUMERIC_TEST", 2)
