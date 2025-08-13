47b28b94d2f198a57c1046b1f4cad161b01f8327
Hannah Recht <hrecht@users.noreply.github.com>
2025-08-13 12:38:35 -0400

    more testing

--- scripts/test-actions.R
+++ scripts/test-actions.R
@@ -8,11 +8,11 @@ send_env <- function(var_name,
 										 send_output = FALSE) {
 	# Convert R booleans to bash booleans
 	if (convert_boolean == TRUE & var_value == TRUE ) {
-		env_part <- paste0(var_name, '=true >> "$GITHUB_ENV"')
+		env_part <- paste0('"', var_name, '=true"', ' >> "$GITHUB_ENV"')
 	}	else if (convert_boolean == TRUE & var_value == FALSE) {
-		env_part <- paste0(var_name, '=false >> "$GITHUB_ENV"')
-	}else {
-		env_part <- paste0(var_name, "=", var_value, ' >> "$GITHUB_ENV"')
+		env_part <- paste0('"', var_name, '=false"', ' >> "$GITHUB_ENV"')
+	} else {
+		env_part <- paste0('"', var_name, "=", var_value, '"', ' >> "$GITHUB_ENV"')
 	}
 	
 	print(env_part)
@@ -22,16 +22,24 @@ send_env <- function(var_name,
 	# Should look like
 	#system(echo "STRING_TEST=${{env.STRING_TEST}}" >> $GITHUB_OUTPUT)
 	if (send_output == TRUE) {
-		output_part <- paste0(var_name, "=", var_value, ' >> "$GITHUB_OUTPUT"')
+		if (convert_boolean == TRUE & var_value == TRUE ) {
+			env_part <- paste0('"', var_name, '=true"', ' >> "$GITHUB_OUTPUT"')
+		}	else if (convert_boolean == TRUE & var_value == FALSE) {
+			env_part <- paste0('"', var_name, '=false"', ' >> "$GITHUB_OUPUT"')
+		} else {
+			output_part <- paste0('"', var_name, "=", var_value, '"', ' >> "$GITHUB_OUTPUT"')
+		}
+		
 		print(output_part)
 		system(paste('echo', output_part))
 	}
 }
 
 send_env("STRING_TEST", "Hello world", send_output = TRUE)
-send_env("BOOLEAN_TEST", FALSE)
-send_env("NUMERIC_TEST", 5)
+send_env("BOOLEAN_TEST", FALSE, send_output = TRUE)
+send_env("NUMERIC_TEST", 5, send_output = TRUE)
 send_env("DATE_TEST", Sys.time())
 
 # Try getting from the env
-Sys.getenv("github.repository")
+repo <- Sys.getenv("github.repository")
+print(repo)
