33c496372e4ecbe44aea11d402fe1db13c9d450a
Hannah Recht <hrecht@users.noreply.github.com>
2025-08-13 10:51:53 -0400

    try rm quotes

--- scripts/test-actions.R
+++ scripts/test-actions.R
@@ -3,7 +3,7 @@
 # Final result should look like:
 # 'echo "POST_BSKY=false" >> "$GITHUB_ENV"'
 send_env <- function(var_name, var_value) {
-	post_part <- paste0(var_name, "='", var_value, "'", ' >> "$GITHUB_ENV"')
+	post_part <- paste0(var_name, "=", var_value, ' >> "$GITHUB_ENV"')
 	print(post_part)
 	system(paste('echo ', post_part))
 }
@@ -11,4 +11,4 @@ send_env <- function(var_name, var_value) {
 send_env("TEXT_TEST", "Hello world")
 send_env("BOOLEAN_TEST", TRUE)
 send_env("NUMERIC_TEST", 2)
-send_env("DATE_TEST", Sys.Date())
+send_env("DATE_TEST", Sys.time())
