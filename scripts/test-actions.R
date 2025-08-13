239bb79e5929336572dab08e15a02c983e9b8de7
Hannah Recht <hrecht@users.noreply.github.com>
2025-08-13 14:32:21 -0400

    no boolean conversion

--- scripts/test-actions.R
+++ scripts/test-actions.R
@@ -4,17 +4,8 @@
 # 'echo "POST_BSKY=false" >> "$GITHUB_ENV"'
 send_env <- function(var_name,
 										 var_value,
-										 convert_boolean = TRUE,
 										 send_output = FALSE) {
-	# Convert R booleans to bash booleans
-	if (convert_boolean == TRUE & var_value == TRUE ) {
-		env_part <- paste0('"', var_name, '=true"', ' >> "$GITHUB_ENV"')
-	}	else if (convert_boolean == TRUE & var_value == FALSE) {
-		env_part <- paste0('"', var_name, '=false"', ' >> "$GITHUB_ENV"')
-	} else {
-		env_part <- paste0('"', var_name, "=", var_value, '"', ' >> "$GITHUB_ENV"')
-	}
-	
+	env_part <- paste0('"', var_name, "=", var_value, '"', ' >> "$GITHUB_ENV"')
 	print(env_part)
 	system(paste('echo', env_part))
 	
@@ -22,24 +13,17 @@ send_env <- function(var_name,
 	# Should look like
 	#system(echo "STRING_TEST=${{env.STRING_TEST}}" >> $GITHUB_OUTPUT)
 	if (send_output == TRUE) {
-		if (convert_boolean == TRUE & var_value == TRUE ) {
-			ouput_part <- paste0('"', var_name, '=true"', ' >> "$GITHUB_OUTPUT"')
-		}	else if (convert_boolean == TRUE & var_value == FALSE) {
-			output_part <- paste0('"', var_name, '=false"', ' >> "$GITHUB_OUTPUT"')
-		} else {
-			output_part <- paste0('"', var_name, "=", var_value, '"', ' >> "$GITHUB_OUTPUT"')
-		}
-		
+		output_part <- paste0('"', var_name, "=", var_value, '"', ' >> "$GITHUB_OUTPUT"')
 		print(output_part)
 		system(paste('echo', output_part))
 	}
 }
 
 send_env("STRING_TEST", "Hello world", send_output = TRUE)
-send_env("BOOLEAN_TEST", FALSE, convert_boolean = FALSE, send_output = TRUE)
+send_env("BOOLEAN_TEST", FALSE, send_output = TRUE)
 send_env("NUMERIC_TEST", 5, send_output = TRUE)
 send_env("DATE_TEST", Sys.time())
-send_env("ARB_TEST", var_value = nrow(mtcars))
+send_env("ARB_TEST", nrow(mtcars))
 send_env("LIST_TEST", var_value = '["foo", "bar"]')
 
 # Try getting from the env
