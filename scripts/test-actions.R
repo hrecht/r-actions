849e92d395a989ba124158f791d168e95e252a05
Hannah Recht <hrecht@users.noreply.github.com>
2025-08-13 16:05:48 -0400

    cleaner

--- scripts/test-actions.R
+++ scripts/test-actions.R
@@ -2,30 +2,45 @@
 
 # Final result should look like:
 # 'echo "POST_BSKY=false" >> "$GITHUB_ENV"'
+
+send_var <- function(var_name,
+										 var_value,
+										 destination) {
+	send_part <- paste0('"', var_name, "=", var_value, '"', ' >> "$', destination, '"')
+	print(send_part)
+	system(paste('echo', send_part))
+}
 send_env <- function(var_name,
 										 var_value,
 										 send_output = FALSE) {
-	env_part <- paste0('"', var_name, "=", var_value, '"', ' >> "$GITHUB_ENV"')
-	print(env_part)
-	system(paste('echo', env_part))
+	
+	send_var(var_name, var_value, "GITHUB_ENV")
 	
 	# Save to Github output for use in future jobs
-	# Should look like
-	#system(echo "STRING_TEST=${{env.STRING_TEST}}" >> $GITHUB_OUTPUT)
 	if (send_output == TRUE) {
-		output_part <- paste0('"', var_name, "=", var_value, '"', ' >> "$GITHUB_OUTPUT"')
-		print(output_part)
-		system(paste('echo', output_part))
+		send_var(var_name, var_value, "GITHUB_OUTPUT")
 	}
 }
 
 send_env("STRING_TEST", "Hello world", send_output = TRUE)
-send_env("BOOLEAN_TEST", FALSE, send_output = TRUE)
+send_env("BOOLEAN_TEST", F, send_output = TRUE)
 send_env("NUMERIC_TEST", 5, send_output = TRUE)
 send_env("DATE_TEST", Sys.time())
 send_env("ARB_TEST", nrow(mtcars))
-send_env(c("LIST_TEST", "LIST_TEST2"), var_value = c("foo", "bar"))
+send_env("LIST_TEST", var_value = '["foo", "bar"]')
 
 # Try getting from the env
 repo <- Sys.getenv("github.repository")
 print(repo)
+
+system('echo
+			 MULTILINE_TEST=$(cat << EOF
+			 					 Line 1
+			 					 Line 2
+			 					 Line 3
+			 					 EOF
+			 )
+			 echo "MULTILINE_TEST<<EOF" >> $GITHUB_ENV
+			 echo "$MULTILINE_TEST" >> $GITHUB_ENV
+			 echo "EOF" >> $GITHUB_ENV')
+
